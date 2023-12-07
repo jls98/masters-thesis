e18f20f6e47c9fa1d05ae43f77d659ca9b3e577e
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2023-12-07 23:02:17 +0100

    t2 fix

--- Cache_Reversing/task2.c
+++ Cache_Reversing/task2.c
@@ -15,7 +15,7 @@ static void wait(const uint64_t cycles);
 static uint64_t lfsr_create(void);
 static uint64_t lfsr_rand(uint64_t* lfsr);
 static uint64_t lfsr_step(uint64_t lfsr);
-static uint64_t probe_stride_loop(const void **addr, const uint64_t addr_len, const uint64_t reps, const uint64_t stride);
+static uint64_t probe_stride_loop(const uint64_t addr_len, const uint64_t reps, const uint64_t stride);
 static unsigned int log2_floor(unsigned int val);
 static void create_pointer_chase(void** addr, const uint64_t size);
 int get_ways(int cache_size);
@@ -46,7 +46,7 @@ int get_ways(int cache_size) {
     create_pointer_chase(buffer, double_cache_size / sizeof(void*));
     // check stride in power of two
     for (uint32_t stride = 0; stride < log2_floor(double_cache_size); stride++) {
-        uint64_t millicycles = probe_stride_loop(buffer, double_cache_size, PROBE_REPS, stride);
+        uint64_t millicycles = probe_stride_loop(double_cache_size, PROBE_REPS, stride);
         printf("stride: %5d; time: %7.3f cycles\n", stride, (double)millicycles/(1<<10));
 
         munmap(buffer, double_cache_size);
