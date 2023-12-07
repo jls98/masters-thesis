96b7a38743280833bd1dc814adbc89a461c4805d
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2023-12-07 23:01:40 +0100

    reps 25 -> 20

--- Cache_Reversing/task2.c
+++ Cache_Reversing/task2.c
@@ -79,24 +79,18 @@ static uint64_t lfsr_step(uint64_t lfsr) {
   return (lfsr & 1) ? (lfsr >> 1) ^ FEEDBACK : (lfsr >> 1);
 }
 
-static uint64_t probe_stride_loop(const void **addr, const uint64_t addr_len, const uint64_t reps, const uint64_t stride){
+static uint64_t probe_stride_loop(const uint64_t addr_len, const uint64_t reps, const uint64_t stride){
     volatile void *ignore;
-    uint64_t lfsr = lfsr_create();
-    uint64_t *lfsr_vals = (uint64_t *) malloc(reps * sizeof(uint64_t));
-    uint64_t mod = addr_len / sizeof(void *);
-    for (uint64_t i=0;i<reps;i++){
-        for (uint64_t j = 0; j< stride; j++){
-            lfsr = lfsr_rand(&lfsr);
-        }
-        lfsr_vals[i] = lfsr % mod;
-    }
+    uint64_t *dump = (uint64_t) malloc(addr_len * sizeof(uint64_t));
+    for (size_t i=0; i<addr_len-1;i++) dump[i] = i;
     
+    uint64_t index=0;
     uint64_t start = rdtsc();
-    for(int i = reps-1; i>=0; i--){
-        ignore = addr[lfsr_vals[i]];
-    }
-    uint64_t diff = rdtsc() - start;
-    return diff / (uint64_t)(reps >> 10);
+    for (uint64_t i=0; i<reps-1;i++){
+        dump[index] *= 3;
+        index=(index+stride) % addr_len;
+    } 
+    return (rdtsc() - start) / (uint64_t)(reps >> 10);
 }
 /*static uint64_t probe_stride_loop(const void *addr, const uint64_t addr_len, const uint64_t reps, const uint64_t stride) {
 	volatile uint64_t time;
