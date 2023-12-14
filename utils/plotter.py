dce6270fc43d27c972e9724caf79ded17135c397
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2023-12-14 19:12:45 +0100

    data

--- utils/plotter.py
+++ utils/plotter.py
@@ -1,6 +1,7 @@
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
 
 def parse_input_file(file_path):
     data = []
@@ -11,17 +12,47 @@ def parse_input_file(file_path):
                 data.append((stride, cycle))
     return np.array(data)
 
-def plot_data(data):
+def format_ticks(value, _):
+    # Custom tick formatter to display powers of 2
+    return f'2^{int(np.log2(value))}'
+    
+def plot_data_task2(data):
     strides, cycles = data[:, 0], data[:, 1]
     
     plt.figure(figsize=(10, 6))
     plt.scatter(strides, cycles, label='cycles', marker='o', color='b')
+    plt.plot(strides, cycles, linestyle='-', color='b', linewidth=0.5)  # Thin line connecting the dots
     plt.yscale('linear')  # Adjust the scale if needed
-    plt.xscale('log')
+    plt.xscale('log', base=2)
     
-    plt.title('Cycle Count vs. Stride')
+    plt.title('Cycle Count per Stride')
     plt.xlabel('Stride')
     plt.ylabel('Cycles')
+    
+    # Ensure a minimum number of ticks on the x-axis
+    plt.gca().xaxis.set_major_formatter(FuncFormatter(format_ticks))
+    
+    plt.grid(True)
+    plt.legend()
+    
+    plt.show()
+    
+def plot_data_task1(data):
+    strides, cycles = data[:, 0], data[:, 1]
+    
+    plt.figure(figsize=(10, 6))
+    plt.scatter(strides, cycles, label='cycles', marker='o', color='b')
+    plt.plot(strides, cycles, linestyle='-', color='b', linewidth=0.5)  # Thin line connecting the dots
+    plt.yscale('linear')  # Adjust the scale if needed
+    plt.xscale('log', base=2)
+    
+    plt.title('Cycle Count by Memsize')
+    plt.xlabel('Memsize')
+    plt.ylabel('Cycles')
+    
+    # Ensure a minimum number of ticks on the x-axis
+    plt.gca().xaxis.set_major_formatter(FuncFormatter(format_ticks))
+    
     plt.grid(True)
     plt.legend()
     
@@ -29,12 +60,26 @@ def plot_data(data):
 
 def main():
     parser = argparse.ArgumentParser(description='Plot data from input file.')
-    parser.add_argument('input', help='Input file containing stride-cycle pairs.')
+    parser.add_argument('filename', help='Input file containing stride-cycle pairs.')
+    parser.add_argument('task', help='task to plot.')
     args = parser.parse_args()
-
-    input_file_path = args.input
+    
+    # put inputs in vars
+    input_file_path = args.filename
+    current_task = args.task
+    
+    # parse data from file
     data = parse_input_file(input_file_path)
-    plot_data(data)
+    
+    # select task
+    if current_task == 'task1':
+        plot_data_task1(data)
+    elif current_task == 'task2':
+        plot_data_task2(data)
+
+         
+        
+    
 
 if __name__ == "__main__":
     main()
