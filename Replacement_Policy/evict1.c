c75eb45298ccee7464920aaf5cb600855fefa715
Jia-Lin <Jia-Lin.Shi@rub.de>
2023-12-15 14:00:21 +0100

    fix

--- Replacement_Policy/evict1.c
+++ Replacement_Policy/evict1.c
@@ -45,7 +45,7 @@ static void control(){
 	uint64_t *adrs = (uint64_t *) malloc(ar_size* sizeof(uint64_t));
 	uint64_t candidate = 64; // heap and stack -> not cached because of locality
 	CREATE_POINTER_STRIDE_CHASE(adrs, ar_size, 1);
-	part = probe(adrs, ar_size+1, adrs[10]);
+	part = probe(adrs, ar_size+1, &adrs[10]);
 	not_part = probe(adrs, ar_size, &candidate);
 	
 	printf("part %lu\nnot part %lu\n%p\n%p\n%p\n%p\n", part, not_part, &part, &not_part, &candidate, adrs);
