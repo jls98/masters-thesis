1f2bdf6af0a7bf90e8df5e91e6b4e4864eddfeae
Jia-Lin <Jia-Lin.Shi@rub.de>
2023-12-21 00:45:07 +0100

    jens new

--- Eviction_Set/jens.c
+++ Eviction_Set/jens.c
@@ -18,7 +18,7 @@ int main(int ac, char **av) {
 
     for (int k = 12; k < 26; k++) {
         int size = 1 << k;
-        void* *buffer = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        void* *buffer = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
 
         create_pointer_chase(buffer, size / sizeof(void*), 1);
         uint64_t millicycles = probe_chase_loop(buffer, PROBE_REPS);
@@ -28,7 +28,7 @@ int main(int ac, char **av) {
     }
 	for (int k = 12; k < 26; k++) {
         int size = 1 << k;
-        void* *buffer = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        void* *buffer = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
 
         create_pointer_chase(buffer, size / sizeof(void*), 2);
         uint64_t millicycles = probe_chase_loop(buffer, PROBE_REPS);
@@ -39,7 +39,7 @@ int main(int ac, char **av) {
 	
 	for (int k = 12; k < 26; k++) {
         int size = 1 << k;
-        void* *buffer = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        void* *buffer = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
 
         create_pointer_chase(buffer, size / sizeof(void*), 3);
         uint64_t millicycles = probe_chase_loop(buffer, PROBE_REPS);
