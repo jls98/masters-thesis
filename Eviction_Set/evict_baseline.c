c0bc7b45a72bfaf00c234a022c9db266a65cad77
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-01-03 22:56:24 +0100

    adding new evic set implementation attempts

---
+++ Eviction_Set/evict_baseline.c
@@ -0,0 +1,34 @@
+#include <stdint.h>
+#include <stdio.h>
+
+
+int main(int ac, char **av){
+    return 0;
+}
+
+
+
+
+static void wait(const uint64_t cycles) {
+	unsigned int ignore;
+	uint64_t start = __rdtscp(&ignore);
+	while (__rdtscp(&ignore) - start < cycles);
+}
+
+#define FEEDBACK 0x80000000000019E2ULL
+static uint64_t lfsr_create(void) {
+  uint64_t lfsr;
+  asm volatile("rdrand %0": "=r" (lfsr)::"flags");
+  return lfsr;
+}
+
+static uint64_t lfsr_rand(uint64_t* lfsr) {
+    for (uint64_t i = 0; i < 8*sizeof(uint64_t); i++) {
+        *lfsr = lfsr_step(*lfsr);
+    }
+    return *lfsr;
+}
+
+static uint64_t lfsr_step(uint64_t lfsr) {
+  return (lfsr & 1) ? (lfsr >> 1) ^ FEEDBACK : (lfsr >> 1);
+}
