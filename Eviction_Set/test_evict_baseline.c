931ecffc1dcf87200890a47462e8658908d8379a
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-01-04 13:11:36 +0100

    pick test

--- Eviction_Set/test_evict_baseline.c
+++ Eviction_Set/test_evict_baseline.c
@@ -2,10 +2,46 @@
 #include <CUnit/CUnit.h>
 #include <CUnit/Basic.h>
 
+void test_pick(){
+    uint64_t set_size = 5;
+    uint64_t size = 6;
+    uint64_t *set = (uint64_t *) malloc(set_size*sizeof(uint64_t *)));
+    uint64_t *base = (uint64_t *) malloc(size*sizeof(uint64_t *)));
+    uint64_t lfsr = lfsr_create();
+    
+    // test uninitialized params
+    
+    CU_ASSERT_EQUAL(pick(set, set_size, base, size, NULL), size+1);
+    CU_ASSERT_EQUAL(pick(set, set_size, NULL, size, lfsr), size+1);
+    CU_ASSERT_EQUAL(pick(NULL, set_size, base, size, lfsr), size+1);
+    
+    
+    // set some arbitrary indexes
+    set[0]=0;
+    set[1]=5;
+    set[2]=2;
+    set[3]=7;
+    set[4]=12;
+    
+    base[0]=5;
+    base[1]=65;
+    base[2]=3;
+    base[3]=10;
+    base[4]=8;
+    base[5]=9;
+    
+    // test no candidate possible
+    // no elements left in base
+    CU_ASSERT_EQUAL(pick(set, set_size, base, 0, lfsr), size+1); 
+    // already in eviction set 
+    CU_ASSERT_EQUAL(pick(set, set_size, base, 1, lfsr), size+1); 
+
+    // regular (in range?) (ASSERT_TRUE)
+    CU_ASSERT_EQUAL(pick(set, set_size, base, 2, lfsr), 65); 
+
+}
 
 void test_create_pointer_chase(){
-
-    
     uint64_t base_size = 512;
     uint64_t set_size = 0;
     void **base = mmap(NULL, base_size * sizeof(void *), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
@@ -22,16 +58,6 @@ void test_create_pointer_chase(){
     set[7] = 205;
     set[8] = 77;
     set[9] = 3;
-    set[10] = 456;
-    set[11] = 29;
-    set[12] = 198;
-    set[13] = 72;
-    set[14] = 431;
-    set[15] = 174;
-    set[16] = 50;
-    set[17] = 111;
-    set[18] = 503;
-    set[19] = 91;
 
     
     // case set size = 0
@@ -42,7 +68,7 @@ void test_create_pointer_chase(){
     
     // case element index out of range
     base_size=511;
-    set_size=20;
+    set_size=10;
     create_pointer_chase(base, base_size, set, set_size);
     
     // regular
@@ -71,6 +97,7 @@ int main() {
 
     CU_pSuite suite = CU_add_suite("Test Suite evict_baseline", NULL, NULL);
     CU_add_test(suite, "Test create_pointer_chase", test_create_pointer_chase);
+    CU_add_test(suite, "Test pick", test_pick);
 
     CU_basic_run_tests();
     CU_cleanup_registry();
