e3863d91b23da7b28e66a2e0d98e7a109b36bfa4
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-01-05 02:26:19 +0100

    changed pick to linkedliust

--- Eviction_Set/test_evict_baseline.c
+++ Eviction_Set/test_evict_baseline.c
@@ -74,7 +74,7 @@ void test_pick(){
     candidate_set = addElement(candidate_set, 69);
     candidate_set = addElement(candidate_set, 66);
     uint64_t val = pick(evict_set, candidate_set, base_size, &lfsr);
-    CU_ASSERT_TRUE(val > 64 && val < base_set); 
+    CU_ASSERT_TRUE(val > 64 && val < base_size); 
 
 
 }
