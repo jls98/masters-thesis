aa995f9aacad3cab0aaf916be993dd6d8744298c
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-01-12 00:21:23 +0100

    probe stridfe loops rework

--- Cache_Reversing/task2.c
+++ Cache_Reversing/task2.c
@@ -43,29 +43,29 @@ int get_ways_sqr(int cache_size) {
 		uint64_t buffer_size = double_cache_size / sizeof(void*);
 		uint64_t stride = 1<<s;
         create_pointer_stride_chase(buffer, buffer_size, stride);  
-        uint64_t reps = double_cache_size % stride == 0? buffer_size/stride : buffer_size/stride +1;
-		printf("s %lu, stride %lu, reps %lu, index %lu, size of pointer chase %lu\n", s, stride, reps, buffer_size, buffer_size/stride);
-        double millicycles = probe_stride_loop(buffer, reps);
+        //uint64_t reps = double_cache_size % stride == 0? buffer_size/stride : buffer_size/stride +1;
+		printf("s %lu, stride %lu, reps %lu, size of pointer chase %lu\n", s, stride, buffer_size, buffer_size/stride);
+        double millicycles = probe_stride_loop(buffer, buffer_size);
         //printf("stride: %5d; time: %7.3f cycles\n", (1<<stride), millicycles);
         printf("%7ld %7.3f\n", 8*stride, millicycles);
 		
         create_pointer_stride_chase(buffer, buffer_size, stride);  
         reps = double_cache_size % stride == 0? buffer_size/stride : buffer_size/stride +1;
-        millicycles = probe_stride_loop(buffer, reps);
-        //printf("stride: %5d; time: %7.3f cycles\n", (1<<stride), millicycles);
-        printf("%7ld %7.3f\n", 8*stride, millicycles);
-		
-		
-        create_pointer_stride_chase(buffer, buffer_size, stride);  
-        reps = double_cache_size % stride == 0? buffer_size/stride : buffer_size/stride +1;
-        millicycles = probe_stride_loop(buffer, reps);
+        double millicycles = probe_stride_loop(buffer, buffer_size);
         //printf("stride: %5d; time: %7.3f cycles\n", (1<<stride), millicycles);
         printf("%7ld %7.3f\n", 8*stride, millicycles);
 		
 		
         create_pointer_stride_chase(buffer, buffer_size, stride);  
         reps = double_cache_size % stride == 0? buffer_size/stride : buffer_size/stride +1;
-        millicycles = probe_stride_loop(buffer, reps);
+        double millicycles = probe_stride_loop(buffer, buffer_size);
+        //printf("stride: %5d; time: %7.3f cycles\n", (1<<stride), millicycles);
+        printf("%7ld %7.3f\n", 8*stride, millicycles);
+		
+		
+        create_pointer_stride_chase(buffer, buffer_size, stride);  
+        reps = double_cache_size % stride == 0? buffer_size/stride : buffer_size/stride +1;
+        double millicycles = probe_stride_loop(buffer, buffer_size);
         //printf("stride: %5d; time: %7.3f cycles\n", (1<<stride), millicycles);
         printf("%7ld %7.3f\n", 8*stride, millicycles);
        
