27513c1edc7dc42e1ecad8210849c4d4136e8ed8
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-01-12 00:54:01 +0100

    task2 finalize

--- Cache_Reversing/task2.c
+++ Cache_Reversing/task2.c
@@ -49,14 +49,6 @@ int get_ways_sqr(int cache_size) {
         //printf("stride: %5d; time: %7.3f cycles\n", (1<<stride), millicycles);
         printf("%7ld %7.3f\n", 8*stride, millicycles);
 		
-		stride = 3*stride;
-        create_pointer_stride_chase(buffer, buffer_size, stride);  
-        //uint64_t reps = double_cache_size % stride == 0? buffer_size/stride : buffer_size/stride +1;
-		//printf("s %lu, stride %lu, reps %lu, size of pointer chase %lu\n", s, stride, buffer_size, buffer_size/stride);
-        millicycles = probe_stride_loop(buffer, buffer_size);
-        //printf("stride: %5d; time: %7.3f cycles\n", (1<<stride), millicycles);
-        printf("%7ld %7.3f\n", 8*stride, millicycles);
-		
        
         munmap(buffer, double_cache_size);
     }
