0186c8095e676428d72905510cc04940b1d7886b
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-01-23 17:47:12 +0100

    prints

--- Eviction_Set/evict_baseline.c
+++ Eviction_Set/evict_baseline.c
@@ -206,7 +206,7 @@ static struct Node * create_minimal_eviction_set(void **candidate_set, uint64_t 
 	printf("a_tmp Elements in eviction set %lu, cind_set empty %i, evict_set empty %i\n", a_tmp, cind_set==NULL, evict_set==NULL);
     /* baseline algorithm */
 	printList(evict_set);
-	return &evict_set;
+	return evict_set;
 }
 
 static void wait(uint64_t cycles) {
