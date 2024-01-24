a3f94e8e8e3b16c5915c5c62072fb0a2c7090a30
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-01-24 19:09:17 +0100

    additional check in cmes

--- Eviction_Set/evict_baseline.c
+++ Eviction_Set/evict_baseline.c
@@ -257,7 +257,7 @@ static struct Node * create_minimal_eviction_set(void **candidate_set, uint64_t 
         for(struct Node* it=combined_set;it!=NULL;cnt++, it=it->next);
         
         // if not TEST(R union S\{c}), x)  if removing c results in not evicting x anymore, add c to current eviction set    
-        if(combined_set->value != NULL && !TEST1(candidate_set[combined_set->value], cnt, victim_adrs)){
+        if(combined_set != NULL && combined_set->value != NULL && !TEST1(candidate_set[combined_set->value], cnt, victim_adrs)){
             evict_set = addElement(evict_set, c);
 			printf("head evict_set: %p\n", evict_set);			
             a_tmp++; // added elem to evict set -> if enough, evict_set complete
