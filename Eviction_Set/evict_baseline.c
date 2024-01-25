1cd8aee6987f2c1bb0d48bc960ba1459a21b7806
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-01-25 20:09:50 +0100

    switch to gen12

--- Eviction_Set/evict_baseline.c
+++ Eviction_Set/evict_baseline.c
@@ -270,7 +270,10 @@ static struct Node * create_minimal_eviction_set(void **candidate_set, uint64_t 
 				}
 				uint64_t time = probe(victim_adrs);
 				printf("Time loading victim after evict set  %lu\n", time);		
-				if(time>THRESHOLD) break;
+				if(time>THRESHOLD) {
+				
+					break;
+				}
 			}
 		
 				
