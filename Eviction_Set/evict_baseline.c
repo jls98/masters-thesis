3f0b1e08ffa0400b7c5781f32fcfafb339e747d3
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-01-25 03:43:04 +0100

    additional check in cmes

--- Eviction_Set/evict_baseline.c
+++ Eviction_Set/evict_baseline.c
@@ -7,7 +7,7 @@
 
 /* threshold values for loading 1 adrs                  */
 // IntelGen12 e core
-#define THRESHOLD_SINGLE_L1D_E12 50      // ~2.2/ 61<60 on single measurement / new 200? 140-180
+#define THRESHOLD_SINGLE_L1D_E12 120      // ~2.2/ 61<60 on single measurement / new 200? 140-180
 #define THRESHOLD_SINGLE_L2_E12 50      // cached ~<18
 #define THRESHOLD_SINGLE_LLC_E12 70     // ~52 (?)
 #define THRESHOLD_SINGLE_DEFAULT_E12 THRESHOLD_SINGLE_L1D_E12
