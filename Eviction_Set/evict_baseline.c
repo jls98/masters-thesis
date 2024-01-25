10acfcd4845934317d6ec0172a0d9b80b26d2de5
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-01-25 12:42:30 +0100

    additional check in cmes

--- Eviction_Set/evict_baseline.c
+++ Eviction_Set/evict_baseline.c
@@ -459,7 +459,7 @@ static int64_t test1(void *addr, uint64_t size, void* cand, uint64_t threshold){
     if(sum/reps<500) times[sum]+=1;
 	else times[500]+=1;
 	
-	if (sum/reps <= threshold)printf("Sum %lu, sum/reps %lu for size %lu\n", sum, sum/reps, size);
+	printf("Sum %lu, sum/reps %lu for size %lu\n", sum, sum/reps, size);
 	return sum/reps > threshold? 1 : 0;
 } /**/
 
