76613235e50469531aed35c46bd83d69783f3a53
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-01-31 18:48:07 +0100

    type

--- Eviction_Set/evict_baseline.c
+++ Eviction_Set/evict_baseline.c
@@ -201,7 +201,7 @@ int main(int ac, char **av){
 
 	// measure time when cached	
 	load(victim_adrs);
-	time = probe(victim_adrs);
+	uinnt64_t time = probe(victim_adrs);
 	
 	printf("Time loading victim cached %lu\n", time);
 	// measure time when uncached	
