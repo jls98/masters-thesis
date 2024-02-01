710f387f156b65d93b68fdf256bfd2f33c0a57c4
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-02-02 00:34:35 +0100

    first experiment

--- Eviction_Set/evict_baseline.c
+++ Eviction_Set/evict_baseline.c
@@ -192,7 +192,8 @@ int main(int ac, char **av){
     void **candidate_set = mmap(NULL, 2* c_size * sizeof(void *), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
 	uint64_t *intptr = (uint64_t *) candidate_set[0];
 	*intptr = 0xff;
-	for (uint64_t i=0;i<9;i++) printf("%lu %x\n", i, candidate_set[i]); // learn about indexing void ** 
+	//for (uint64_t i=0;i<9;i++) 
+	printf("%lu %x\n", 0, candidate_set[0]); // learn about indexing void ** 
 	for(uint64_t i=0;i<c_size;i++) candidate_set[i] = &candidate_set[i]; // avoid null page by writing something on every entry (pointer to itself)
 	
     void *target_adrs = &candidate_set[c_size+1000]; // take target somewhere in the middle of allocated memory
