56333b53ca95b67fe3e1a652978223d3756ebc25
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-02-02 00:37:43 +0100

    first experiment

--- Eviction_Set/evict_baseline.c
+++ Eviction_Set/evict_baseline.c
@@ -190,7 +190,7 @@ int main(int ac, char **av){
 
     // map candidate_set (using hugepages, twice the size of cache in bytes (4 times to have space for target))
     void **candidate_set = mmap(NULL, 2* c_size * sizeof(void *), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
-	//uint64_t *intptr = (uint64_t *) candidate_set[0];
+	uint64_t *intptr = (uint64_t *) candidate_set[0];
 	//*intptr = 0xff;
 	//for (uint64_t i=0;i<9;i++) 
 	//printf("%lu %x\n", 0, candidate_set[0]); // learn about indexing void ** 
