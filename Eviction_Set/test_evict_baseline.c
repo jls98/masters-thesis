948762d4e1686c347b4b91097e3cf762c665e943
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-02-02 17:26:05 +0100

    added index+8

--- Eviction_Set/test_evict_baseline.c
+++ Eviction_Set/test_evict_baseline.c
@@ -2,7 +2,7 @@
 #include <CUnit/CUnit.h>
 #include <CUnit/Basic.h>
 
-#define reps_test1 1000
+#define reps_test1 100
 
 void test_test1(){
 	struct Config *conf = initConfig(8, 64, 54, 32768, 200);
