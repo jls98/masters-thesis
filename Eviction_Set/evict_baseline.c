452076947388e4244575ca7a58d62f3924edbd3f
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-02-02 12:50:32 +0100

    extended test

--- Eviction_Set/evict_baseline.c
+++ Eviction_Set/evict_baseline.c
@@ -191,8 +191,8 @@ int main(int ac, char **av){
     // map candidate_set (using hugepages, twice the size of cache in bytes (4 times to have space for target))
     void **candidate_set = mmap(NULL, 10* c_size * sizeof(void *), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
 	
-    void *target_adrs = &candidate_set[7400]; // take target somewhere in the middle of allocated memory
-	printf("c[] %p and &c[] %p \n", candidate_set[7400], &candidate_set[7400]);
+    void *target_adrs = &candidate_set[7464]; // take target somewhere in the middle of allocated memory
+	printf("c[] %p and &c[] %p \n", candidate_set[7464], &candidate_set[7464]);
 	// create handmade eviction set: M memory addresses = 32768/8=4096, S sets = 64 -> stride of 64 in indexes
 	for(int i=296;i<3881;i+=512){ // 296, 808, 1320, 1832, 2344, 2856, 3368, 3880  index +1 == 8 bytes
 		evict_set = addElement(evict_set, i); 
