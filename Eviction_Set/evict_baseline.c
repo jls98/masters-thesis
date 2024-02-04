bfacb5b77734d4a5293fc5416eb7f7237d0d426d
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-02-04 15:46:52 +0100

    changed reps

--- Eviction_Set/evict_baseline.c
+++ Eviction_Set/evict_baseline.c
@@ -639,13 +639,12 @@ static int64_t pick(struct Node* candidate_set, uint64_t base_size, uint64_t *lf
 	} 
 	
     uint64_t c=-1, j, c_size; // c picked candidate, j index in candidate_set, c_size current candidate set size
-    struct Node *cur_node;
     // get candidate set size  
     // iterate over all elements and count
     //for(cur_node = candidate_set, c_size=0; cur_node != NULL; c_size++, cur_node = cur_node->next);
     c_size = (uint64_t) count(candidate_set);
     
-    for(j = lfsr_rand(lfsr) % c_size, cur_node = candidate_set;j>0;j--, cur_node=cur_node->next) c=cur_node->value;
+    for(j = lfsr_rand(lfsr) % c_size, struct Node *cur_node = candidate_set;j>1;j--, cur_node=cur_node->next) c=cur_node->value;
     
     
     
