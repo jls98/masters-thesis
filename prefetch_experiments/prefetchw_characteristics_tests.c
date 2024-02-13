dd585da43ecc17f234371c03737c92b654bc728a
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-02-13 15:13:34 +0100

    reorder

--- prefetch_experiments/prefetchw_characteristics_tests.c
+++ prefetch_experiments/prefetchw_characteristics_tests.c
@@ -29,7 +29,7 @@ void cpuid(){
 int main()
 {
 	void *map_read = mmap(NULL, 64, PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-	((uint64_t *) map_read) = 1;
+	*((uint64_t *) map_read) = 1;
     _mm_clflush(map_read);
     _mm_mfence();
     uint64_t time = probe(map_read);
