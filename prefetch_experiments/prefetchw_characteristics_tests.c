a50177dbd3075d6560594667af7bcd311067c320
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-02-13 14:50:02 +0100

    fences

--- prefetch_experiments/prefetchw_characteristics_tests.c
+++ prefetch_experiments/prefetchw_characteristics_tests.c
@@ -10,30 +10,31 @@ uint64_t probe(void *adrs){
         " mfence            \n"
         " lfence            \n"
         " rdtscp             \n"
-        " mov %rax, %r8 		\n"
-        " mov (%%1), %rax		\n"
+        " mov r8, rax 		\n"
+        " mov rax, [%1]		\n"
         " lfence            \n"
         " rdtscp             \n"
-        " sub %r8, %rax 		\n"
+        " sub rax, r8 		\n"
         : "=&a" (time)
         : "r" (adrs)
-        : "%ecx", "%rdx", "%r8", "memory"
+        : "ecx", "rdx", "r8", "memory"
 	);
 	return time;
-}
-
-void flush(void *adrs){
-	__asm__ volatile("clflush [%0];lfence" ::"r" (adrs));
 }
 
 int main()
 {
 	void *map_read = mmap(NULL, 64, PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-	flush(map_read);
+	_mm_clflush(map_read);
+    _mm_mfence();
     printf("Hello World %lu\n", probe(map_read));
+    _mm_mfence();
     printf("Hello World %lu\n", probe(map_read));
-	flush(map_read);
+	_mm_mfence();
+    _mm_clflush(map_read);
+    _mm_mfence();
 	_m_prefetchw(map_read);
+    _mm_mfence();
     printf("Hello World %lu\n", probe(map_read));
     return 0;
 }
