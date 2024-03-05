c0fe8ad2ac0c1fb89a4ec955c6d14584c1584d80
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-03-05 04:41:27 +0100

    test pp init

--- PP/test_pp.c
+++ PP/test_pp.c
@@ -5,12 +5,19 @@
 
 
 void test_pp_init(){
+	ptinf("\n\nStart test_pp_init ... \n")
+	
 	// no conf
 	CU_ASSERT_PTR_NULL(pp_init(NULL));
 	
 	Config *conf=initConfig(-1,-1,-1,-1,-1,-1, -1,0); // init conf with default values and buffer size of 0
 	CU_ASSERT_PTR_NULL(pp_init(conf));
 	
+	
+	Config *conf=initConfig(-1,-1,-1,-1,-1,-1, -1,1);
+	CU_ASSERT_PTR_NOT_NULL(pp_init(conf))
+	printf("End test_pp_init\n\n")
+
 }
 
 int main(int ac, char **av) {
