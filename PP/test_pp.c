e0af85aee0d2ee9869a9dddf485234afb42cdb96
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-03-05 04:46:44 +0100

    test pp init

--- PP/test_pp.c
+++ PP/test_pp.c
@@ -13,30 +13,30 @@ void test_pp_init(){
 	CU_ASSERT_PTR_NULL(pp_init(NULL));
 	
 	Config *conf=initConfig(-1,-1,-1,-1,-1,-1, -1,0); // init conf with default values and buffer size of 0
-	return_value=pp_init(conf)
+	return_value=pp_init(conf);
 	CU_ASSERT_PTR_NULL(return_value);
 	
 	// works
 	conf=initConfig(-1,-1,-1,-1,-1,-1, -1,1);
-	return_value=pp_init(conf)
+	return_value=pp_init(conf);
 	CU_ASSERT_PTR_NULL(return_value);
 	munmap(return_value, 1);
 	
 	// limit testing
 	conf=initConfig(-1,-1,-1,-1,-1,-1, -1,512*4096);
-	return_value=pp_init(conf)
+	return_value=pp_init(conf);
 	CU_ASSERT_PTR_NULL(return_value);
 	munmap(return_value, 512*4096);
 	
 	// limit testing
 	conf=initConfig(-1,-1,-1,-1,-1,-1, -1,4096*4096);
-	return_value=pp_init(conf)
+	return_value=pp_init(conf);
 	CU_ASSERT_PTR_NULL(return_value);
 	munmap(return_value, 4096*4096);
 	
 	// limit testing
 	conf=initConfig(-1,-1,-1,-1,-1,-1, -1,16000000);
-	return_value=pp_init(conf)
+	return_value=pp_init(conf);
 	CU_ASSERT_PTR_NULL(return_value);
 	munmap(return_value, 16000000);
 
