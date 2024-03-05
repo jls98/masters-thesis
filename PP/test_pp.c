3c65c9963c1c1c473841d9828d83c0752c63092e
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-03-05 04:46:28 +0100

    test pp init

--- PP/test_pp.c
+++ PP/test_pp.c
@@ -7,27 +7,39 @@
 void test_pp_init(){
 	printf("\n\nStart test_pp_init ... \n");
 	
+	void *return_value;
+	
 	// no conf
 	CU_ASSERT_PTR_NULL(pp_init(NULL));
 	
 	Config *conf=initConfig(-1,-1,-1,-1,-1,-1, -1,0); // init conf with default values and buffer size of 0
-	CU_ASSERT_PTR_NULL(pp_init(conf));
+	return_value=pp_init(conf)
+	CU_ASSERT_PTR_NULL(return_value);
 	
 	// works
 	conf=initConfig(-1,-1,-1,-1,-1,-1, -1,1);
-	CU_ASSERT_PTR_NOT_NULL(pp_init(conf));
+	return_value=pp_init(conf)
+	CU_ASSERT_PTR_NULL(return_value);
+	munmap(return_value, 1);
 	
 	// limit testing
 	conf=initConfig(-1,-1,-1,-1,-1,-1, -1,512*4096);
-	CU_ASSERT_PTR_NOT_NULL(pp_init(conf));
+	return_value=pp_init(conf)
+	CU_ASSERT_PTR_NULL(return_value);
+	munmap(return_value, 512*4096);
 	
 	// limit testing
 	conf=initConfig(-1,-1,-1,-1,-1,-1, -1,4096*4096);
-	CU_ASSERT_PTR_NOT_NULL(pp_init(conf));
+	return_value=pp_init(conf)
+	CU_ASSERT_PTR_NULL(return_value);
+	munmap(return_value, 4096*4096);
 	
 	// limit testing
 	conf=initConfig(-1,-1,-1,-1,-1,-1, -1,16000000);
-	CU_ASSERT_PTR_NOT_NULL(pp_init(conf));
+	return_value=pp_init(conf)
+	CU_ASSERT_PTR_NULL(return_value);
+	munmap(return_value, 16000000);
+
 	printf("End test_pp_init\n\n");
 
 }
