c88b61c8e5f02b5c15e9d085181b1b131773cd06
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-03-07 03:57:34 +0100

    msr

--- utils/utils.c
+++ utils/utils.c
@@ -195,13 +195,12 @@ static void my_fence(){
 	__asm__ volatile("mfence");
 }
 
-static void set_msr_bits(int32_t value){
-	int32_t to_and = 0xfff0+value;
+static void set_msr_bits(i64 value){
+	i64 to_and = 0xfffffff0+value;
 	__asm__ volatile(
-	"mov eax, [0x1a4];" 
+	"mov eax, 0x1a4;" 
 	"and eax, %0;" 
-	"mov [0x1a4], rax;"
-	: 
+	"mov 0x1a4, rax;"
 	: "r" (to_and)
 	: "eax", "memory");
 
