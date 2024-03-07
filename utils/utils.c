92ded0030e3e05746e42f9d2d8b43d25d95531ab
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-03-07 01:18:35 +0100

    marker

--- utils/utils.c
+++ utils/utils.c
@@ -122,9 +122,8 @@ static void createPointerChaseInEvictionSet(Eviction_Set *evset){
 	
 	// marker array with all indexes
 	u64 counter=0;
-	int *marker=malloc(sizeof(int)*evset->size);
+	int *marker=calloc(evset->size, sizeof(int));
 	// memset(marker, 0, evset->size*sizeof(int));
-	for(int i=0;i<evset->size;i++) marker[i]=1;
 	u64 index_next=rng%evset->size, index_current=0;
 	u64 *pointer_current= (u64 *)evset->adrs[index_current], pointer_next= (u64 *)evset->adrs[index_next];
 	while(counter!=evset->size){ // repeat until all evset adrs are set
