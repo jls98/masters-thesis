7aec66638f0dfd4fe30d717e39ddac6ca6854cb6
Jia-Lin <Jia-Lin.Shi@rub.de>
2024-03-12 00:23:05 +0100

    swapped load1

--- Replacement_Policy/cache8way.c
+++ Replacement_Policy/cache8way.c
@@ -147,6 +147,35 @@ void test_L1_cache(){
 		printf("Time sum of element %i at %p is %lu, avg per iteration is %lu\n", i, evset->adrs[i], evset->measurements[i], evset->measurements[i]/TEST_REPS);
 		evset->measurements[i]=0;
 	}	
+		
+	printf("\n1 element loaded from target set:\n");
+	for(int j=0;j<TEST_REPS;j++){
+		
+		for(int i=0;i<8;i++) {
+			flush(target_set->adrs[i]);
+			flush(evset->adrs[i]);
+		}
+		my_fence();
+		for(int i=0;i<8;i++){
+			my_fence();
+			pp_probe(evset); // load all evset elems
+			my_fence();
+			pp_probe(target_set); // load "1" target adrs 
+			my_fence();
+			do{
+				time = probe(evset->adrs[i]);
+			} while(time > OUTSIDER_TRESHOLD);			
+			my_fence();
+			evset->measurements[i] += time;
+			my_fence();
+			
+			// printf("Time of element %i is %lu\n", i, time);
+		}
+	}
+	for(int i=0;i<8;i++){ // print and reset measurements
+		printf("Time sum of element %i is %lu, avg per iteration is %lu\n", i, evset->measurements[i], evset->measurements[i]/TEST_REPS);
+		evset->measurements[i]=0;
+	}	
 	
 	printf("\n1 element loaded from loaded target set:\n");
 	for(int j=0;j<TEST_REPS;j++){
