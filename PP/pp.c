14273bb68b5a26acd05494d11c9e272731404ba8
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-03-14 08:36:56 +0100

    probe tests

--- PP/pp.c
+++ PP/pp.c
@@ -118,7 +118,7 @@ static void pp_setup(Config *conf, Eviction_Set *evset, void *target, void *cc_b
     intptr_t buf = (intptr_t) cc_buffer;
     intptr_t tar = (intptr_t) target;    
     i64 offset = 4096 - (abs(((i64) cc_buffer) - ((i64) target)) % 4096);
-    printf("computed offset %li from buf %p and tar %p\n", offset, buf, tar);
+    // printf("computed offset %li from buf %p and tar %p\n", offset, buf, tar);
 	for(u64 i=0;i<conf->cache_ways;i++){
 		// add multiple of pagesize to ensure to land in the same cache set
 		addEvictionAdrs(evset, cc_buffer+offset+(i+1)*conf->pagesize); // void * -> exakt index value
@@ -138,7 +138,10 @@ static void pp_monitor(Config *conf, Eviction_Set *evset, void *target) {
 	printf("probe target is %li\n", probe(target));
     
 	printf("probe is %li\n", pp_probe(evset));
-    
+	printf("probe is %li\n", pp_probe(evset));
+	printf("probe is %li\n", pp_probe(evset));
+	printf("probe target is %li\n", probe(target));
+   
     printf("target %p\n",target);
     for(int i=0;i<evset->size;i++){
         printf("evset[%i] %p\n",i, evset->adrs[i]);
