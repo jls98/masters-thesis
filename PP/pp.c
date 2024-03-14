b2cd3a2494be78aa56f248c84a7bb01ce1e01974
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-03-14 07:56:22 +0100

    probe tests

--- PP/pp.c
+++ PP/pp.c
@@ -130,6 +130,8 @@ static void pp_monitor(Config *conf, Eviction_Set *evset, void *target) {
 	printf("probe is %li\n", pp_probe(evset));
 	load(target);
 	printf("probe is %li\n", pp_probe(evset));
+	printf("probe target is %li\n", probe(target));
+    
 	printf("probe is %li\n", pp_probe(evset));
 }
 
