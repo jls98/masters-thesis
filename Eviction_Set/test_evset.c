9fb322dfd7a0aea676cea0cc015eb5722cd2dcfe
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-03-28 15:17:22 -0400

    toggle

--- Eviction_Set/test_evset.c
+++ Eviction_Set/test_evset.c
@@ -18,7 +18,7 @@ void test_node(){
     for(int i=0;i<10;i++){
         index=i;
         list_append(&nodes, &nodes[i]);
-        CU_ASSERT_EQUAL(nodes[i], list_get(&nodes, &index));
+        CU_ASSERT_EQUAL(&nodes[i], list_get(&nodes, &index));
 
     }
     
@@ -27,24 +27,24 @@ void test_node(){
 
 
 int main(int ac, char **av) {
-	if (ac==1) conf = initConfig(8, 64, 41, 32768, 1, 1); // default L1 lab machine, no inputs
-	if (ac==2){
-		int conf_choice = strtol(av[1], NULL, 10);
-		if (conf_choice==11) conf = initConfig(8, 64, 54, 32768, 1, 1); 	// L1 i7
-		if (conf_choice==12) conf = initConfig(8, 64, 58, 262144, 1, 1); 	// L2 i7 what value?
-		if (conf_choice==13) conf = initConfig(8, 64, 58, 262144, 1, 1); 	// L3 i7 TODO or unneeded
+	// if (ac==1) conf = initConfig(8, 64, 41, 32768, 1, 1); // default L1 lab machine, no inputs
+	// if (ac==2){
+		// int conf_choice = strtol(av[1], NULL, 10);
+		// if (conf_choice==11) conf = initConfig(8, 64, 54, 32768, 1, 1); 	// L1 i7
+		// if (conf_choice==12) conf = initConfig(8, 64, 58, 262144, 1, 1); 	// L2 i7 what value?
+		// if (conf_choice==13) conf = initConfig(8, 64, 58, 262144, 1, 1); 	// L3 i7 TODO or unneeded
 		
-		if (conf_choice==21) conf = initConfig(8, 64, 75, 32768, 1, 1); 	// L1e i12
-		if (conf_choice==22) conf = initConfig(8, 64, 85, 2097152, 1, 1); 	// L2e i12 TODO
-		if (conf_choice==23) conf = initConfig(8, 64, 58, 262144, 1, 1); 	// L3e i12 TODO or unneeded
-		if (conf==NULL){
-			printf("Error, no valid choice, XY, whereby X is the CPU (1:i7, 2:i12) and Y the cache Level (1-3)");
-			return 0;
-		}
-	}
-	if (ac==7){ // self, ways, cache line size, threshold, cache size, test reps, hugepages
-		conf = initConfig(strtol(av[1], NULL, 10), strtol(av[2], NULL, 10), strtol(av[3], NULL, 10), strtol(av[4], NULL, 10), strtol(av[5], NULL, 10), strtol(av[6], NULL, 10));
-	}
+		// if (conf_choice==21) conf = initConfig(8, 64, 75, 32768, 1, 1); 	// L1e i12
+		// if (conf_choice==22) conf = initConfig(8, 64, 85, 2097152, 1, 1); 	// L2e i12 TODO
+		// if (conf_choice==23) conf = initConfig(8, 64, 58, 262144, 1, 1); 	// L3e i12 TODO or unneeded
+		// if (conf==NULL){
+			// printf("Error, no valid choice, XY, whereby X is the CPU (1:i7, 2:i12) and Y the cache Level (1-3)");
+			// return 0;
+		// }
+	// }
+	// if (ac==7){ // self, ways, cache line size, threshold, cache size, test reps, hugepages
+		// conf = initConfig(strtol(av[1], NULL, 10), strtol(av[2], NULL, 10), strtol(av[3], NULL, 10), strtol(av[4], NULL, 10), strtol(av[5], NULL, 10), strtol(av[6], NULL, 10));
+	// }
 	
     CU_initialize_registry();
 
