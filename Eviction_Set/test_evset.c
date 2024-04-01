fd19b05d59a2a58d26f33d9212ab59991b42db23
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-04-01 12:24:13 -0400

    lala2 8

--- Eviction_Set/test_evset.c
+++ Eviction_Set/test_evset.c
@@ -93,22 +93,22 @@ void test_test(){
     __asm__ volatile("lfence;");
     for(int i=0;i<REPS;i++){
         for(int i=offset;i<LALALALAL2+offset;i++){
-            access(&test[i*8192]);
+            access(&test[i*2*8192]);
         }
         for(int i=offset;i<LALALALAL2+offset;i++){
-            access(&test[i*8192]);
+            access(&test[i*2*8192]);
         }
         for(int i=offset;i<LALALALAL2+offset;i++){
-            access(&test[i*8192]); // sth is still odd :()
+            access(&test[i*2*8192]); // sth is still odd :()
         }
         for(int i=offset;i<LALALALAL2+offset;i++){
-            access(&test[i*8192]);
+            access(&test[i*2*8192]);
         }
         for(int i=offset;i<LALALALAL2+offset;i++){
-            access(&test[i*8192]);
+            access(&test[i*2*8192]);
         }
         for(int i=offset;i<LALALALAL2+offset;i++){
-            access(&test[i*8192]); // sth is still odd :()
+            access(&test[i*2*8192]); // sth is still odd :()
         }
         probe(((void *)test)+222);    
         __asm__ volatile("lfence;");
