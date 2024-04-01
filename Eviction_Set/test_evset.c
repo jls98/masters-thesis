f5c836c489566268fbfdee7aaec57dac671cabe3
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-04-01 17:14:17 -0400

    debug print

--- Eviction_Set/test_evset.c
+++ Eviction_Set/test_evset.c
@@ -111,7 +111,7 @@ void test_test(){
     printf("asd\n");
     index = 1024+INDEX_OFFSET;
     Node *tmp = list_get(buffer_ptr, &index);
-    printf("asd\n");
+    printf("asd %p %p\n" tmp, &tmp);
     Node **head1=&tmp;
     printf("asd\n");
     index =262144 + INDEX_OFFSET;
