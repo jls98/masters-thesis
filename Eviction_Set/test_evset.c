86fcfc453a5d216d4db747589e06cc53fdcd5e22
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-04-01 14:30:37 -0400

    lala2 8

--- Eviction_Set/test_evset.c
+++ Eviction_Set/test_evset.c
@@ -86,13 +86,13 @@ void test_test(){
     __asm__ volatile("lfence;");
     for(int i=0;i<REPS;i++){
         for(int i=offset;i<LALALALAL2+offset;i++){
-            access(&test[i*1024]);
+            access(&test[i*2048]);
         }
         for(int i=offset;i<LALALALAL2+offset;i++){
-            access(&test[i*1024]);
+            access(&test[i*2048]);
         }
         for(int i=offset;i<LALALALAL2+offset;i++){
-            access(&test[i*1024]);
+            access(&test[i*2048]);
         }
         probe(((void *)test)+222);    
         __asm__ volatile("lfence;");
