26df443c697b413af321d542316f146dad9873d9
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-04-03 16:11:42 -0400

    list print

--- Eviction_Set/test_evset.c
+++ Eviction_Set/test_evset.c
@@ -147,6 +147,7 @@ void test_test(){
         list_append(head1, tmp);
         list_print(head1);
     }    
+    printf("oh\n");
     printf("8th elem %p\n", list_get(head1, 7));
 
     for(int i=1;i<LALALALAL2;i++){
