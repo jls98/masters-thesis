4a29b562c85c73b1158597c2ce0550e52d7ee3a5
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-04-03 20:14:45 -0400

    list0 pad

--- Eviction_Set/evset.c
+++ Eviction_Set/evset.c
@@ -424,7 +424,7 @@ static u64 test_intern(Node *ptr, u64 size, void *target){
 
     access(target);
     access(target);
-    __asm__ volatile ("lfence;");
+    // __asm__ volatile ("lfence;");
     traverse_list0(ptr, size);
     
     // victim + 222 access for page walk
