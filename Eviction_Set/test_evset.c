a7496fa1e73a2d94d582e1df031bb835d7e623b4
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-04-04 18:15:19 -0400

    mmap pad

--- Eviction_Set/test_evset.c
+++ Eviction_Set/test_evset.c
@@ -180,7 +180,7 @@ void test_test(){
     munmap(buffer, size_factor*sizeof(Node));    
     
 }
-#define AMOUNT_HISTO 99999999
+#define AMOUNT_HISTO 9999999
 void test_get_histogram_data(){
     for(int size=5;size<25;size++){
         Node *buffer = (Node *) mmap(NULL, AMOUNT_HISTO*sizeof(Node), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
