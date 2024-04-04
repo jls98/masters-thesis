d031a0665042e94a60d2658760e1b65beb0e26bb
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-04-03 20:18:20 -0400

    tar acc pad

--- Eviction_Set/evset.c
+++ Eviction_Set/evset.c
@@ -422,6 +422,8 @@ static u64 test_intern(Node *ptr, u64 size, void *target){
 
     access(target);
     access(target);
+    access(target);
+    access(target);
     __asm__ volatile ("lfence;");
     traverse_list0(ptr, size);
     
