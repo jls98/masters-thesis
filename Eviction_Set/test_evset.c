a282c238dd307e980b6d5d171d6fca89712294f0
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-04-04 16:00:54 -0400

    added histogram scirpt pad

--- Eviction_Set/test_evset.c
+++ Eviction_Set/test_evset.c
@@ -182,7 +182,7 @@ void test_test(){
 }
 
 void test_get_histogram_data(){
-    for(int size=5;size<50;size++){
+    for(int size=5;size<40;size++){
         Node *buffer = (Node *) mmap(NULL, size_factor*sizeof(Node), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
         list_init(buffer, size_factor*sizeof(Node));
         Node **buffer_ptr=&buffer;
