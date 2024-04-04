7eb1e4c92bb9d60418bf92700642dc94efe58b2a
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-04-04 16:32:04 -0400

    increased reps pad

--- Eviction_Set/test_evset.c
+++ Eviction_Set/test_evset.c
@@ -183,7 +183,7 @@ void test_test(){
 
 void test_get_histogram_data(){
     for(int size=5;size<25;size++){
-        Node *buffer = (Node *) mmap(NULL, 99999999*sizeof(Node), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
+        Node *buffer = (Node *) mmap(NULL, 9999999*sizeof(Node), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
         list_init(buffer, size_factor*sizeof(Node));
         Node **buffer_ptr=&buffer;
         u64 index;
@@ -211,7 +211,7 @@ void test_get_histogram_data(){
             printf("%i %lu\n", size, msrmts[i]);
         }      
         msr_index=0;
-        munmap(buffer, 99999999*sizeof(Node)); 
+        munmap(buffer, 9999999*sizeof(Node)); 
     }
 }
 
