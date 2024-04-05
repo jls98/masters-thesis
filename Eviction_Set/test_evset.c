e51626eba263be3758f0cfbed292481b45a40096
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-04-05 15:39:53 -0400

    stride pad

--- Eviction_Set/test_evset.c
+++ Eviction_Set/test_evset.c
@@ -237,10 +237,12 @@ void test_strides(){
             return;
         }
         Node **buffer_ptr=&buffer;
+        printf("list init now\n");
         list_init(buffer, size*sizeof(Node));
         
         Node *tmp;
         u64 index;
+        printf("append now\n");
         for(int i=0;i<size/(stride*sizeof(Node));i++){
             index=i*stride-i;
             // printf("index %lu stride %i\n", index, stride);
