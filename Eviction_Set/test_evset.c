cdfa4a318b70f0a68e4db4f7c705d423f78c1a54
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-04-05 18:35:43 -0400

    print alginment pad

--- Eviction_Set/test_evset.c
+++ Eviction_Set/test_evset.c
@@ -223,7 +223,7 @@ void test_get_histogram_data(){
 #define ALDER_L2 2097152
 #define SKYLAKE_L2 262144
 #define CACHE_L1 32768
-#define SIZE_VALUE ALDER_L2
+#define SIZE_VALUE CACHE_L1
 
 void test_strides(){
     // stride * 2^5 since sizeof(Node) = 32
@@ -315,7 +315,6 @@ int main(int ac, char **av) {
 	// // free(conf);
     // test_test();
 	// test_get_histogram_data();
-    printf("strides\n");
     
     test_strides();
     return 0;
