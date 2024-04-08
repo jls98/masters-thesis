b1dd3855211c6945be68c7d13d0ebcbe751f7018
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-04-08 19:03:44 -0400

    test test 8192 16 size pad

--- Eviction_Set/test_evset.c
+++ Eviction_Set/test_evset.c
@@ -123,7 +123,7 @@ void testbench_skylake_evsets(){
 
 #define REPS1 5
 #define INDEX_OFFSET 10
-#define EVSET1_SIZE 8
+#define EVSET1_SIZE 16
 void test_test(){
     Node *buffer = (Node *) mmap(NULL, size_factor*sizeof(Node), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
     list_init(buffer, size_factor*sizeof(Node));
