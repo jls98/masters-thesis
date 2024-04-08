032eed5bd8676a878ae3d8a0c83689228dd44311
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-04-08 17:04:51 -0400

    file gen pad

--- utils/file_generator.c
+++ utils/file_generator.c
@@ -16,7 +16,7 @@ void generateZeroFile(const char *file_name, size_t file_size) {
     for (size_t i = 0; i < file_size; i++) {
         randint = rand() % 256;
         sprintf(buf, "%d", randint);
-        fputc(buf, file);
+        fputc(*buf, file);
     }
     free(buf);
     fclose(file);
