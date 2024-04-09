d32bca420d700e5c58eab071c6c059b60dc3d1f8
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-04-09 16:01:14 -0400

    fixed size in madvise pad

--- Eviction_Set/test_evset.c
+++ Eviction_Set/test_evset.c
@@ -173,7 +173,7 @@ void test_test(){
     for(int i=0;i<REPS1;i++){
         printf("b: %lu\n", msrmts[i]);
     }
-    munmap(buffer, size_factor*sizeof(Node));    
+    munmap(buffer, PAGESIZE);    
     
 }
 #define AMOUNT_HISTO 9999999
