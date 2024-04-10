b45831d8e626b8467aa386e6bef27fa2a1e8907b
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-04-10 09:46:48 -0400

    cache line new vals pad

--- Eviction_Set/test_evset.c
+++ Eviction_Set/test_evset.c
@@ -359,7 +359,7 @@ int main(int ac, char **av) {
     CU_basic_run_tests();
     CU_cleanup_registry();
 	// test_get_histogram_data();
-    test_strides();
+    // test_strides();
     cache_line();
     return 0;
 }
