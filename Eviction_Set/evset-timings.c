069cf6e673486cfdb005c1ac13f12afb033e2b9a
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2024-04-17 16:04:13 -0400

    static acs rm prints pad

--- Eviction_Set/evset-timings.c
+++ Eviction_Set/evset-timings.c
@@ -555,26 +555,26 @@ static u64 static_accesses(Node *buffer, u64 total_size, u64 reps){
 static void timings(){
     wait(1E9);
     // allocate 
-    Node *buffer= (Node *) mmap(NULL, 8*PAGESIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
+    Node *buffer= (Node *) mmap(NULL, 4*PAGESIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
     if(buffer==MAP_FAILED){
         printf("mmap failed\n");
         return;
     }
     
-    if (madvise(buffer, 8*PAGESIZE, MADV_HUGEPAGE) ==-1){
+    if (madvise(buffer, 4*PAGESIZE, MADV_HUGEPAGE) ==-1){
         printf("advise failed!\n");
         return;
     }
-    list_init(buffer, 8*PAGESIZE);
+    list_init(buffer, 4*PAGESIZE);
     Node *tmp;
     u64 total_time=0;
     u64 total_size;
 
-    total_size = 16777216;
-    printf("\ntotal size %lu\n", total_size);
-    total_time=static_accesses(buffer, total_size, TOTALACCESSES);
+    // total_size = 16777216;
+    // printf("\ntotal size %lu\n", total_size);
+    // total_time=static_accesses(buffer, total_size, TOTALACCESSES);
 
-    printf("total time %lu, avg %lu\n", total_time, total_time/TOTALACCESSES);
+    // printf("total time %lu, avg %lu\n", total_time, total_time/TOTALACCESSES);
     
     total_size = 8388608;
     printf("\ntotal size %lu\n", total_size);
