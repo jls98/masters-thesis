3894c06ea3c4364c5d6f2dd5b1cb82c089e16c3d
jls98 <shi.jialin98@gmail.com>
2024-05-09 20:46:42 +0200

    added two cases

--- Eviction_Set/victim.c
+++ Eviction_Set/victim.c
@@ -1,5 +1,3 @@
-
-
 void victim_loop(){
     for(int i=0;i<1000000;i++){
         asm __volatile__("mfence;");
@@ -10,24 +8,48 @@ void victim_loop(){
     }
 }
 
-void victim_call(){
+void victim_loop2(){
+    for(int i=0;i<1000000;i++){
+        asm __volatile__("mfence;");
+        i+=2;
+        i*=2;
+        i-=4;
+        i/=2;
+    }
+}
+
+void victim_call0(){
     victim_loop();
     victim_loop();
     victim_loop();
     victim_loop();
+}
+
+void victim_call1(){
+    victim_loop2();
+    victim_loop2();
+    victim_loop2();
+    victim_loop2();
 }
 
 void victim_call2(){
-    victim_call();
-    victim_call();
-    victim_call();
-    victim_call();
+    victim_call1();
+    victim_call1();
+    victim_call1();
+    victim_call1();
 }
 
+void victim_call3(){
+    victim_call0();
+    victim_call0();
+    victim_call0();
+    victim_call0();
+}
 
 void main(){
     victim_call2();
     victim_call2();
     victim_call2();
     victim_call2();
+    // victim_call3();
 }
